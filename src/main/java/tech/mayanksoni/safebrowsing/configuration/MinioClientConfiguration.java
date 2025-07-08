90977f4e41f286a85a1a8657e35edab9db5a16bb
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-07-08 09:17:12 -0600

    improve(logging): Updated the logging structure with key id being logged.
    Signed-off-by: Mayank Soni <mayank.soni@mayanksoni.tech>

--- src/main/java/tech/mayanksoni/safebrowsing/configuration/MinioClientConfiguration.java
+++ src/main/java/tech/mayanksoni/safebrowsing/configuration/MinioClientConfiguration.java
@@ -2,6 +2,7 @@ package tech.mayanksoni.safebrowsing.configuration;
 
 import io.minio.MinioClient;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @AllArgsConstructor
 @Getter
 @Setter
+@Slf4j
 @ConfigurationProperties(prefix = "minio")
 public class MinioClientConfiguration {
     private String endpoint;
@@ -20,6 +22,7 @@ public class MinioClientConfiguration {
 
     @Bean
     public MinioClient minioClient() {
+        log.info("Initializing Minio client using Access Key ID :{}", accessKey);
         return MinioClient.builder()
                 .endpoint(endpoint)
                 .credentials(accessKey, secretKey)
